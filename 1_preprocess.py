4902401496985146a748f898503683d23318f311
Hang Yao <hang_yao@hotmail.com>
2016-07-02 07:40:06 -0500

    feat: changed validation dataset
    Changed the way to create validation dataset.
    
    Composed the validation set with 2/3 from training samples (400 per class) and 1/3 from extra samples (200 per class), yielding a total of 6000 samples.

--- 1_preprocess.py
+++ 1_preprocess.py
@@ -1,3 +1,4 @@
+
 from __future__ import print_function
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,13 +9,10 @@ from IPython.display import display, Image
 from scipy import ndimage
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.cross_validation import StratifiedShuffleSplit
 from six.moves.urllib.request import urlretrieve
 from six.moves import cPickle as pickle
 get_ipython().magic(u'matplotlib inline')
 
-
-# Download Data Files
 
 url = 'http://ufldl.stanford.edu/housenumbers/'
 last_percent_reported = None
@@ -48,14 +46,11 @@ def maybe_download(filename, force=False):
 train_filename = maybe_download('train.tar.gz')
 test_filename = maybe_download('test.tar.gz')
 
-# Download mat Data Files
 
 train_matfile = maybe_download('train_32x32.mat')
 test_matfile = maybe_download('test_32x32.mat')
 extra_matfile = maybe_download('extra_32x32.mat')
 
-
-# Load mat Data Files
 
 import scipy.io
 train_data = scipy.io.loadmat('train_32x32.mat', variable_names='X').get('X')
@@ -65,78 +60,77 @@ test_labels = scipy.io.loadmat('test_32x32.mat', variable_names='y').get('y')
 extra_data = scipy.io.loadmat('extra_32x32.mat', variable_names='X').get('X')
 extra_labels = scipy.io.loadmat('extra_32x32.mat', variable_names='y').get('y')
 
-#print(train_data.shape, train_labels.shape)
-#print(test_data.shape, test_labels.shape)
-#print(extra_data.shape, extra_labels.shape)
+print(train_data.shape, train_labels.shape)
+print(test_data.shape, test_labels.shape)
+print(extra_data.shape, extra_labels.shape)
 
-# Stratified Shuffle Validation Data Set
 
-random_state = 43
-y = extra_labels[:,0]
-index = StratifiedShuffleSplit(y, 10, test_size=25000, random_state=random_state)
-for _, valid_index in index:
-    valid_data, valid_labels = extra_data[:,:,:,valid_index], extra_labels[valid_index]
+# Build Validation Dataset and Labels Based on the Methods in This Paper:
+# [https://arxiv.org/pdf/1204.3968.pdf]
 
-#print(valid_data.shape)
-#print(valid_labels.shape)
+import random
 
-# Check Validation Images
+random.seed()
 
-#num_image = 24567
-#plt.imshow(valid_data[:,:,:,num_image])
-#print(valid_labels[num_image])
+n_labels = 10
+valid_index = []
+valid_index2 = []
+train_index = []
+for i in np.arange(n_labels):
+    valid_index.extend(np.where(train_labels[:,0] == (i+1))[0][:400].tolist())
+    train_index.extend(np.where(train_labels[:,0] == (i+1))[0][400:].tolist())
+    valid_index2.extend(np.where(extra_labels[:,0] == (i+1))[0][:200].tolist())
 
-# Check Validation Dataset Balance
+random.shuffle(valid_index)
+random.shuffle(train_index)
+random.shuffle(valid_index2)
 
-#num_labels = [None] * 10
-#for i in range(10):
-#    num_labels[i] = sum(valid_labels == i+1)
-#
-#print(sum(num_labels))
-#print(num_labels)
+valid_data = np.concatenate((train_data[:,:,:,valid_index], extra_data[:,:,:,valid_index2]), axis=3)
+valid_labels = np.concatenate((train_labels[valid_index,:], extra_labels[valid_index2,:]), axis=0)
+train_data_t = train_data[:,:,:,train_index]
+train_labels_t = train_labels[train_index,:]
 
-# Normalize Image Data and Convert to Grayscale
-# Reference the publication below for RGB to Grayscale Conversion:
-# [http://www.eyemaginary.com/Rendering/TurnColorsGray.pdf]
+print(train_data_t.shape, train_labels_t.shape)
+print(test_data.shape, test_labels.shape)
+print(valid_data.shape, valid_labels.shape)
+
 
 image_size = 32  # Pixel width and height.
 pixel_depth = 255.0  # Number of levels per pixel.
 
 def image_convert(image):
-    
+    '''Normalize images and convert RGB to Grayscale'''
     image_data_normal = (image.astype(float) - pixel_depth / 2) / pixel_depth
     image_data = np.ndarray((image.shape[3], image_size, image_size), dtype=np.float32)
     for i in range(image.shape[3]):
-        image_data[i,:,:] = 0.2989 * image_data_normal[:,:,0,i] + \
-          0.5870 * image_data_normal[:,:,1,i] + 0.1140 * image_data_normal[:,:,2,i]
+        # Use the Conversion Method in This Paper:
+        # [http://www.eyemaginary.com/Rendering/TurnColorsGray.pdf]
+        image_data[i,:,:] = 0.2989 * image_data_normal[:,:,0,i] +        0.5870 * image_data_normal[:,:,1,i] + 0.1140 * image_data_normal[:,:,2,i]
     return image_data
 
-train_data_convert = image_convert(train_data)
-test_data_convert = image_convert(test_data)
-valid_data_convert = image_convert(valid_data)
-train_labels_convert = train_labels[:,0]
-test_labels_convert = test_labels[:,0]
-valid_labels_convert = valid_labels[:,0]
+train_data_c = image_convert(train_data_t)
+test_data_c = image_convert(test_data)
+valid_data_c = image_convert(valid_data)
+train_labels_c = train_labels_t[:,0]
+test_labels_c = test_labels[:,0]
+valid_labels_c = valid_labels[:,0]
 
-# Check Converted Image
+print(train_data_c.shape, train_labels_c.shape)
+print(test_data_c.shape, test_labels_c.shape)
+print(valid_data_c.shape, valid_labels_c.shape)
 
-#plt.imshow(valid_data_convert[num_image,:,:])
-#print(valid_labels_convert[num_image])
-
-
-# Save Converted Data Set in a Pickle File
 
 pickle_file = 'SVHN.pickle'
 
 try:
   f = open(pickle_file, 'wb')
   save = {
-    'train_dataset': train_data_convert,
-    'train_labels': train_labels_convert,
-    'valid_dataset': valid_data_convert,
-    'valid_labels': valid_labels_convert,
-    'test_dataset': test_data_convert,
-    'test_labels': test_labels_convert,
+    'train_dataset': train_data_c,
+    'train_labels': train_labels_c,
+    'valid_dataset': valid_data_c,
+    'valid_labels': valid_labels_c,
+    'test_dataset': test_data_c,
+    'test_labels': test_labels_c,
     }
   pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
   f.close()
@@ -147,17 +141,26 @@ except Exception as e:
 statinfo = os.stat(pickle_file)
 print('Compressed pickle size:', statinfo.st_size)
 
-# Run a Logistic Regression on Training and Test Datasets
+
+plt.rcParams['figure.figsize'] = (15.0, 15.0)
+f, ax = plt.subplots(nrows=1, ncols=10)
+
+for i, j in enumerate(np.random.randint(0, train_labels_c.shape[0], size=10)):
+    ax[i].axis('off')
+    ax[i].set_title(train_labels_c[j], loc='center')
+    ax[i].imshow(train_data_c[j,:,:])
+
 
 # Create a Logistic Regression Classifier
-clf = LogisticRegression(penalty='l2', tol=0.0001, C=1.0, random_state=random_state, solver='sag', max_iter=100,multi_class='ovr', verbose=0, n_jobs=4)
+clf = LogisticRegression(penalty='l2', tol=0.0001, C=1.0, random_state=None, solver='sag', max_iter=100,multi_class='ovr', verbose=0, n_jobs=4)
 
-clf.fit(train_data_convert.reshape(train_data_convert.shape[0],-1), train_labels_convert)
+clf.fit(train_data_c.reshape(train_data_c.shape[0],-1), train_labels_c)
+train_prediction = clf.predict(train_data_c.reshape(train_data_c.shape[0],-1))
+test_prediction = clf.predict(test_data_c.reshape(test_data_c.shape[0],-1))
 
+print('Classification report of training data:\n', classification_report(train_labels_c, train_prediction))
+print('Confusion Matrix of training data:\n', confusion_matrix(train_labels_c, train_prediction))
 
-predicted = clf.predict(test_data_convert.reshape(test_data_convert.shape[0],-1))
-
-# Print Results
-print(classification_report(test_labels_convert, predicted))
-print(confusion_matrix(test_labels_convert, predicted))
+print('Classification report of training data:\n', classification_report(test_labels_c, test_prediction))
+print('Confusion Matrix of training data:\n', confusion_matrix(test_labels_c, test_prediction))
 
